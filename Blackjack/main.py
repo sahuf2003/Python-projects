ad31bc2662a77d4a7b8308fac136568774fb32de
sahuf2003 <127684377+sahuf2003@users.noreply.github.com>
2023-12-29 16:36:30 +0530

    Update main.py

--- Blackjack/main.py
+++ Blackjack/main.py
@@ -61,7 +61,7 @@ def play_game():
     if user_score == 0 or computer_score == 0 or user_score > 21:
       is_game_over = True
     else:
-      #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+     
       user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
       if user_should_deal == "y":
         user_cards.append(deal_card())
